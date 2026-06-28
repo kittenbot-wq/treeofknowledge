f9440207617462519887c5348f13c2880af77c3c
Seren <damnkittenbot@gmail.com>
2026-06-28 01:08:47 -0700

    Add files via upload

---
+++ sw-1.js
@@ -0,0 +1,9 @@
+const CACHE = 'kittenbot-peptide-v2';
+const ASSETS = ['./index.html','./banner.png','./icon192.png','./icon512.png','./manifest.json'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if(url.hostname.includes('youtube') || url.hostname.includes('ytimg') || url.hostname.includes('google')) return; // don't cache video/3rd-party
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
